13c25e817a6f23d866fb74519c54fd1082a9d240
tuckeremulls <tuckeremulls@gmail.com>
2021-07-15 11:06:17 -0400

    Initial commit

---
+++ src/main/java/com/apiteam/ticketdemoapp/TicketDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.apiteam.ticketdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiteam/ticketdemoapp/TicketDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiteam.ticketdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
